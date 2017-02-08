14609fcf9273fe09c9d659a1f3212f976ae6ae51
Giusepe Casagrande <giusepe@gmail.com>
2017-02-08 18:54:49 -0200

    First Commit

---
+++ src/ScrollLoginTest/ScrollLoginTestPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace ScrollLoginTest
+{
+    public partial class ScrollLoginTestPage : ContentPage
+    {
+        public ScrollLoginTestPage()
+        {
+            InitializeComponent();
+            LoginButton.Clicked += (sender, e) => DisplayAlert("Button Clicked", "Button Clicked", "Ok");
+        }
+    }
+}
