66b2e2c5bf67da865af6a2aff011d01d8649c8ea
Brandon Minnick <brminnick@users.noreply.github.com>
2017-02-10 11:23:15 -0800

    Added CustomReturnEntry, Removed Grid From Login Page (#1)

---
+++ src/iOS/CustomReturnEntryRenderer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using ScrollLoginTest;
+using ScrollLoginTest.iOS;
+
+[assembly: ExportRenderer(typeof(CustomReturnEntry), typeof(CustomReturnEntryRenderer))]
+namespace ScrollLoginTest.iOS
+{
+	[Preserve(AllMembers = true)]
+	public class CustomReturnEntryRenderer : EntryRenderer
+	{
+		/// <summary>
+		/// Used for registration with the dependency service
+		/// </summary>
+		/// <returns></returns>
+		public new static void Init()
+		{
+			var temp = DateTime.Now;
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			var customEntry = Element as CustomReturnEntry;
+
+			if (Control != null && customEntry != null)
+			{
+				Control.ReturnKeyType = GetKeyboardButtonType(customEntry.ReturnType);
+
+				Control.ShouldReturn += (UITextField tf) =>
+				{
+					customEntry?.ReturnCommand?.Execute(null);
+					return true;
+				};
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == CustomReturnEntry.ReturnTypeProperty.PropertyName)
+			{
+				var customEntry = sender as CustomReturnEntry;
+
+				if (Control != null && customEntry != null)
+					Control.ReturnKeyType = GetKeyboardButtonType(customEntry.ReturnType);
+			}
+
+		}
+
+		UIReturnKeyType GetKeyboardButtonType(ReturnType returnType)
+		{
+			switch (returnType)
+			{
+				case ReturnType.Go:
+					return UIReturnKeyType.Go;
+				case ReturnType.Next:
+					return UIReturnKeyType.Next;
+				case ReturnType.Send:
+					return UIReturnKeyType.Send;
+				case ReturnType.Search:
+					return UIReturnKeyType.Search;
+				case ReturnType.Done:
+					return UIReturnKeyType.Done;
+				default:
+					return UIReturnKeyType.Default;
+			}
+		}
+	}
+}
